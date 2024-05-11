a284b6463fbbb631ef0c043ced1af025e9a4121c
NbekWeb <nbekdev@gmail.com>
2024-05-11 12:57:44 +0500

    add kiosk-order

---
+++ src/pages/dashboard/kiosk-board/DashboardKioskBoardListView.vue
@@ -0,0 +1,126 @@
+<script setup>
+import PageHeaderComponent from '@/components/PageHeaderComponent.vue'
+import { computed, onMounted, ref } from 'vue'
+import useKioskBoard from '@/store/kiosk-board.pinia.js'
+import { useRoute, useRouter } from 'vue-router'
+
+import BoardTableComponent from '@/pages/dashboard/kiosk-board/components/KioskBoardTableComponent.vue'
+import { storeToRefs } from 'pinia'
+import useCore from '@/store/core.pinia.js'
+import useBoardCategory from '@/store/board-category.pinia.js'
+
+const route = useRoute()
+const router = useRouter()
+
+const corePinia = useCore()
+const boardPinia = useKioskBoard()
+const boardCategoryPinia = useBoardCategory()
+
+const { loadingUrl } = storeToRefs(corePinia)
+const { statuses } = storeToRefs(boardPinia)
+const { categories } = storeToRefs(boardCategoryPinia)
+
+const currentPage = computed(() =>
+  route.query.page ? parseInt(route.query.page, 10) : 1
+)
+const category = ref(null)
+const status = ref(null)
+const search = ref(null)
+
+const timeout = ref()
+
+function handleChangeFilter() {
+  router.push({
+    query: {
+      category: category.value,
+      status: status.value,
+      search: search.value
+    }
+  })
+  boardPinia.getAllKioskBoards(
+    currentPage.value - 1,
+    search.value,
+    status.value,
+    category.value
+  )
+}
+function handleSearch() {
+  if (timeout.value) {
+    clearTimeout(timeout.value)
+  }
+  timeout.value = setTimeout(() => {
+    handleChangeFilter()
+  }, 500)
+}
+onMounted(() => {
+  boardPinia.getAllKioskBoardStatus(currentPage.value - 1)
+  boardPinia.getAllKioskBoardStatus()
+  boardCategoryPinia.getAllBoardCategories()
+})
+</script>
+
+<template>
+  <page-header-component :title="$t('DashboardBoardListView')">
+    <template #actions>
+      <a-space>
+        <a-input
+          placeholder="Kanalni izlash..."
+          v-model:value="search"
+          allow-clear
+          @keydown="handleSearch"
+        />
+        <a-tree-select
+          v-model:value="category"
+          class="board-category-filter select"
+          :loading="loadingUrl.has('board/category/all')"
+          :dropdown-style="{
+            maxHeight: '1000px',
+            overflow: 'auto'
+          }"
+          :placeholder="$t('SELECT_CATEGORY')"
+          size="middle"
+          allow-clear
+          tree-default-expand-all
+          :tree-data-simple-mode="[categories]"
+          :tree-data="categories"
+          tree-node-filter-prop="label"
+          @change="handleChangeFilter"
+        >
+        </a-tree-select>
+
+        <a-select
+          @change="handleChangeFilter"
+          v-model:value="status"
+          placeholder="Status"
+          class="select"
+          allow-clear
+        >
+          <a-select-option
+            v-for="(item, i) in statuses"
+            :key="i"
+            :value="item.boardStatus"
+          >
+            {{ item.localName }}
+          </a-select-option>
+        </a-select>
+        <a-button
+          type="primary"
+          class="btn"
+          @click="router.push({ name: 'DashboardKioskPostCreateFormView' })"
+        >
+          <template #icon>
+            <IconPlus />
+          </template>
+          {{ $t('ADD') }} sa1
+        </a-button>
+      </a-space>
+    </template>
+  </page-header-component>
+  <board-table-component />
+</template>
+
+<style scoped lang="scss">
+.select {
+  width: 200px;
+}
+</style>
